a59d36bf8de0a026eafc14eadfcda5178f88a929
Uriel Fiori <uriel.fiori@gmail.com>
2022-06-23 16:18:56 -0300

    initial ABI test

---
+++ source/test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+
+internal class Program
+{
+  private static readonly HttpClient client = new HttpClient();
+  static async Task Main(string[] args)
+  {
+    await ProcessRepositories();
+  }
+
+  private static async Task ProcessRepositories()
+  {
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+    client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+    var stringTask = client.GetStringAsync("http://localhost:7000/ABI/api/ad/obter-usuario/marcelopel");
+
+    var msg = await stringTask;
+    Console.Write(msg);
+  }
+}
