21590791058e813ef54539fd625ce41ba295cd06
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-08 15:07:34 -0300

    removed previous solution

--- source/backend/GPR.Domain/Participante.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GPR.Domain;
-
-public class Participante
-{
-    public int Id { get; set; }
-    public string DiretoriaArea { get; set; } = string.Empty;
-    public string Nome { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-}
