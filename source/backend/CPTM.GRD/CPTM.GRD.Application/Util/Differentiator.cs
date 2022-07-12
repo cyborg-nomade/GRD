34ef56de58ef0793152f7cf6b3eaa6b2f2d2b91c
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-12 16:00:11 -0300

    added util class to create diff string for logs

---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Util/Differentiator.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using KellermanSoftware.CompareNetObjects;
+
+namespace CPTM.GRD.Application.Util;
+
+public static class Differentiator
+{
+    public static string GetDifferenceString<T>(T oldObj, T newObj)
+    {
+        var sb = new StringBuilder();
+        var propertyCount = typeof(T).GetProperties().Length;
+
+        var basicComparison = new CompareLogic()
+            { Config = new ComparisonConfig() { MaxDifferences = propertyCount } };
+        List<Difference> diffs = basicComparison.Compare(oldObj, newObj).Differences;
+
+        foreach (var diff in diffs)
+        {
+            sb.AppendLine("Property name:" + diff.PropertyName);
+            sb.AppendLine("Old value:" + diff.Object1Value);
+            sb.AppendLine("New value:" + diff.Object2Value + "\n");
+        }
+
+        return sb.ToString();
+    }
+}
