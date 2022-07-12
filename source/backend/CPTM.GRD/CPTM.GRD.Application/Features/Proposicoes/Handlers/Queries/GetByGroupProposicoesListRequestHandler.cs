5adf33af558e1bd2b9bb27cc356121e55f502291
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-12 16:36:16 -0300

    idem pt2

--- source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Proposicoes/Handlers/Queries/GetByGroupProposicoesListRequestHandler.cs
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Proposicoes/Handlers/Queries/GetByGroupProposicoesListRequestHandler.cs
@@ -2,6 +2,7 @@
 using CPTM.GRD.Application.DTOs.Main.Proposicao;
 using CPTM.GRD.Application.Features.Proposicoes.Requests.Queries;
 using CPTM.GRD.Application.Persistence.Contracts;
+using CPTM.GRD.Application.Persistence.Contracts.AccessControl;
 using MediatR;
 
 namespace CPTM.GRD.Application.Features.Proposicoes.Handlers.Queries;
@@ -10,18 +11,29 @@ public class
     GetByGroupProposicoesListRequestHandler : IRequestHandler<GetByGroupProposicoesListRequest, List<ProposicaoListDto>>
 {
     private readonly IProposicaoRepository _proposicaoRepository;
+    private readonly IGroupRepository _groupRepository;
     private readonly IMapper _mapper;
 
-    public GetByGroupProposicoesListRequestHandler(IProposicaoRepository proposicaoRepository, IMapper mapper)
+    public GetByGroupProposicoesListRequestHandler(IProposicaoRepository proposicaoRepository,
+        IGroupRepository groupRepository, IMapper mapper)
     {
         _proposicaoRepository = proposicaoRepository;
+        _groupRepository = groupRepository;
         _mapper = mapper;
     }
 
     public async Task<List<ProposicaoListDto>> Handle(GetByGroupProposicoesListRequest request,
         CancellationToken cancellationToken)
     {
-        var proposicoes = await _proposicaoRepository.GetByGroup(request.Gid);
-        return _mapper.Map<List<ProposicaoListDto>>(proposicoes);
+        var groupsToRetrive = await _groupRepository.GetSubordinateGroups(request.Gid);
+        var proposicoes = new List<ProposicaoListDto>();
+        foreach (var group in groupsToRetrive)
+        {
+            var groupProposicoes =
+                await _proposicaoRepository.GetByGroup(group.Id);
+            proposicoes.AddRange(_mapper.Map<List<ProposicaoListDto>>(groupProposicoes));
+        }
+
+        return proposicoes;
     }
 }
