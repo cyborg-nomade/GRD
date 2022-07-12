9be63f45ea0c1c8085af050832f4a2a9fa9986c2
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-12 16:47:13 -0300

    updated change status handler to include logs

--- source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Proposicoes/Handlers/Commands/ChangeStatusProposicaoRequestHandler.cs
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Proposicoes/Handlers/Commands/ChangeStatusProposicaoRequestHandler.cs
@@ -2,6 +2,11 @@
 using CPTM.GRD.Application.DTOs.Main.Proposicao;
 using CPTM.GRD.Application.Features.Proposicoes.Requests.Commands;
 using CPTM.GRD.Application.Persistence.Contracts;
+using CPTM.GRD.Application.Persistence.Contracts.AccessControl;
+using CPTM.GRD.Application.Persistence.Contracts.Logging;
+using CPTM.GRD.Application.Util;
+using CPTM.GRD.Common;
+using CPTM.GRD.Domain.Logging;
 using MediatR;
 
 namespace CPTM.GRD.Application.Features.Proposicoes.Handlers.Commands;
@@ -9,20 +14,37 @@ namespace CPTM.GRD.Application.Features.Proposicoes.Handlers.Commands;
 public class ChangeStatusProposicaoRequestHandler : IRequestHandler<ChangeStatusProposicaoRequest, ProposicaoDto>
 {
     private readonly IProposicaoRepository _proposicaoRepository;
+    private readonly ILogProposicaoRepository _logProposicaoRepository;
+    private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
 
-    public ChangeStatusProposicaoRequestHandler(IProposicaoRepository proposicaoRepository, IMapper mapper)
+    public ChangeStatusProposicaoRequestHandler(IProposicaoRepository proposicaoRepository,
+        ILogProposicaoRepository logProposicaoRepository, IUserRepository userRepository, IMapper mapper)
     {
         _proposicaoRepository = proposicaoRepository;
+        _logProposicaoRepository = logProposicaoRepository;
+        _userRepository = userRepository;
         _mapper = mapper;
     }
 
 
     public async Task<ProposicaoDto> Handle(ChangeStatusProposicaoRequest request, CancellationToken cancellationToken)
     {
-        var proposicao = await _proposicaoRepository.Get(request.Id);
-        proposicao.Status = request.NewStatus;
-        var updatedProposicao = await _proposicaoRepository.Update(proposicao);
+        var savedProposicao = await _proposicaoRepository.Get(request.Id);
+
+        var changeStatusLog = new LogProposicao()
+        {
+            Data = DateTime.Now,
+            Tipo = request.TipoLogProposicao,
+            Diferenca = $@"Changed status from {savedProposicao.Status} to {request.NewStatus}",
+            ProposicaoId = $@"IDPRD: {savedProposicao.IdPrd}",
+            UsuarioResp = await _userRepository.Get(request.Uid),
+        };
+        savedProposicao.Logs.Add(changeStatusLog);
+        await _logProposicaoRepository.Add(changeStatusLog);
+
+        savedProposicao.Status = request.NewStatus;
+        var updatedProposicao = await _proposicaoRepository.Update(savedProposicao);
         return _mapper.Map<ProposicaoDto>(updatedProposicao);
     }
 }
