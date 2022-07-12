4a650661165710b3d9e7c0e723cfacd7dea38a58
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-12 17:26:47 -0300

    created add to reuniao feature

---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Proposicoes/Requests/Commands/AddToReuniaoProposicaoRequest.cs
@@ -0,0 +1,12 @@
+﻿using CPTM.GRD.Application.DTOs.Main.Mixed;
+using CPTM.GRD.Application.DTOs.Main.Proposicao;
+using MediatR;
+
+namespace CPTM.GRD.Application.Features.Proposicoes.Requests.Commands;
+
+public class AddToReuniaoProposicaoRequest : IRequest<AddToPautaDto>
+{
+    public int Pid { get; set; }
+    public int Rid { get; set; }
+    public int Uid { get; set; }
+}
