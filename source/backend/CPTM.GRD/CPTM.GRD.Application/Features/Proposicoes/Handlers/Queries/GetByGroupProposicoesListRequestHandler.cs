adfd43335e0d717120d61b08a74776ba0879c879
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-12 14:56:00 -0300

    added contract methods and request handlers to get proposicao by group and by user

---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Proposicoes/Handlers/Queries/GetByGroupProposicoesListRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CPTM.GRD.Application.DTOs.Main.Proposicao;
+using CPTM.GRD.Application.Features.Proposicoes.Requests.Queries;
+using CPTM.GRD.Application.Persistence.Contracts;
+using MediatR;
+
+namespace CPTM.GRD.Application.Features.Proposicoes.Handlers.Queries;
+
+public class
+    GetByGroupProposicoesListRequestHandler : IRequestHandler<GetByGroupProposicoesListRequest, List<ProposicaoListDto>>
+{
+    private readonly IProposicaoRepository _proposicaoRepository;
+    private readonly IMapper _mapper;
+
+    public GetByGroupProposicoesListRequestHandler(IProposicaoRepository proposicaoRepository, IMapper mapper)
+    {
+        _proposicaoRepository = proposicaoRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<List<ProposicaoListDto>> Handle(GetByGroupProposicoesListRequest request,
+        CancellationToken cancellationToken)
+    {
+        var proposicoes = await _proposicaoRepository.GetByGroup(request.Gid);
+        return _mapper.Map<List<ProposicaoListDto>>(proposicoes);
+    }
+}
--- source/backend/CPTM.GRD/CPTM.GRD.Application/Persistence/Contracts/IProposicaoRepository.cs
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Persistence/Contracts/IProposicaoRepository.cs
@@ -5,9 +5,11 @@ namespace CPTM.GRD.Application.Persistence.Contracts;
 
 public interface IProposicaoRepository : IGenericRepository<Proposicao>
 {
+    Task<IReadOnlyList<Proposicao>> GetByStatus(ProposicaoStatus status);
+    Task<IReadOnlyList<Proposicao>> GetByUser(int uid);
+    Task<IReadOnlyList<Proposicao>> GetByGroup(int gid);
     Task<IReadOnlyList<Proposicao>> GetByUserAndStatus(int uid, ProposicaoStatus status);
     Task<IReadOnlyList<Proposicao>> GetByGroupAndStatus(int gid, ProposicaoStatus status);
-    Task<IReadOnlyList<Proposicao>> GetByStatus(ProposicaoStatus status);
     Task<IReadOnlyList<Proposicao>> GetByReuniao(int rid);
     Task<IReadOnlyList<Proposicao>> GetByReuniaoPrevia(int rid);
 }
