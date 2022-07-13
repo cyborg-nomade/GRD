cc15b09fd8e3695fbb02a7ec9c5cd55d89440e3c
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-13 13:55:09 -0300

    GetReuniaoDetailRequest

---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Reunioes/Handlers/Queries/GetReuniaoDetailRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CPTM.GRD.Application.DTOs.Main.Reuniao;
+using CPTM.GRD.Application.Features.Reunioes.Requests.Queries;
+using CPTM.GRD.Application.Persistence.Contracts;
+using MediatR;
+
+namespace CPTM.GRD.Application.Features.Reunioes.Handlers.Queries;
+
+public class GetReuniaoDetailRequestHandler : IRequestHandler<GetReuniaoDetailRequest, ReuniaoDto>
+{
+    private readonly IReuniaoRepository _reuniaoRepository;
+    private readonly IMapper _mapper;
+
+    public GetReuniaoDetailRequestHandler(IReuniaoRepository reuniaoRepository, IMapper mapper)
+    {
+        _reuniaoRepository = reuniaoRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<ReuniaoDto> Handle(GetReuniaoDetailRequest request, CancellationToken cancellationToken)
+    {
+        var reuniao = await _reuniaoRepository.Get(request.Id);
+        return _mapper.Map<ReuniaoDto>(reuniao);
+    }
+}
---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Reunioes/Requests/Queries/GetReuniaoDetailRequest.cs
@@ -0,0 +1,9 @@
+﻿using CPTM.GRD.Application.DTOs.Main.Reuniao;
+using MediatR;
+
+namespace CPTM.GRD.Application.Features.Reunioes.Requests.Queries;
+
+public class GetReuniaoDetailRequest : IRequest<ReuniaoDto>
+{
+    public int Id { get; set; }
+}
