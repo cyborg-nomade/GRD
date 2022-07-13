74aae92d7d6ea1862a7d4d6d3a079dc83d6049f9
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-13 17:03:03 -0300

    CreatePautaPreviaReuniaoRequest - copia todas as proposições em pauta pra pauta prévia - muda o status de todas as proposições para previa

--- source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Reunioes/Requests/Commands/CreatePautaPreviaReuniaoRequest.cs
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/Reunioes/Requests/Commands/CreatePautaPreviaReuniaoRequest.cs
@@ -5,4 +5,6 @@ namespace CPTM.GRD.Application.Features.Reunioes.Requests.Commands;
 
 public class CreatePautaPreviaReuniaoRequest : IRequest<ReuniaoDto>
 {
+    public int Rid { get; set; }
+    public int Uid { get; set; }
 }
