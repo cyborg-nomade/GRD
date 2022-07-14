64ee8da006205fb18beb62066597d6736c680df9
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-14 13:08:22 -0300

    GetByGroupUsersListRequest

---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/AccessControl/Requests/Queries/GetByGroupUsersListRequest.cs
@@ -0,0 +1,9 @@
+﻿using CPTM.GRD.Application.DTOs.AccessControl;
+using MediatR;
+
+namespace CPTM.GRD.Application.Features.AccessControl.Requests.Queries;
+
+public class GetByGroupUsersListRequest : IRequest<List<UserDto>>
+{
+    public int Gid { get; set; }
+}
