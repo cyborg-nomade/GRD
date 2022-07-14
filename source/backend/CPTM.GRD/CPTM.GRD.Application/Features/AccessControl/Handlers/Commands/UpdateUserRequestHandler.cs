ed89fc0af54e0cddd4d4784bf6677860a90c9fe8
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-14 13:28:23 -0300

    UpdateUserRequest

---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/AccessControl/Handlers/Commands/UpdateUserRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CPTM.GRD.Application.Contracts.Persistence.AccessControl;
+using CPTM.GRD.Application.DTOs.AccessControl;
+using CPTM.GRD.Application.Features.AccessControl.Requests.Commands;
+using MediatR;
+
+namespace CPTM.GRD.Application.Features.AccessControl.Handlers.Commands;
+
+public class UpdateUserRequestHandler : IRequestHandler<UpdateUserRequest, UserDto>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UpdateUserRequestHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<UserDto> Handle(UpdateUserRequest request, CancellationToken cancellationToken)
+    {
+        var savedUser = await _userRepository.Get(request.UserDto.Id);
+        _mapper.Map(request.UserDto, savedUser);
+        var updatedUser = await _userRepository.Update(savedUser);
+        return _mapper.Map<UserDto>(updatedUser);
+    }
+}
