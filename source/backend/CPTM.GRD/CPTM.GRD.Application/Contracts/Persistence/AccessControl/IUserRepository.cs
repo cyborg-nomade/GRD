f02a604df660133d756ae81d0ddcbba059cf5bbe
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-14 13:05:41 -0300

    GetByLevelUsersListRequest

--- source/backend/CPTM.GRD/CPTM.GRD.Application/Contracts/Persistence/AccessControl/IUserRepository.cs
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Contracts/Persistence/AccessControl/IUserRepository.cs
@@ -1,7 +1,11 @@
-﻿using CPTM.GRD.Domain.AccessControl;
+﻿using CPTM.GRD.Common;
+using CPTM.GRD.Domain.AccessControl;
 
 namespace CPTM.GRD.Application.Contracts.Persistence.AccessControl;
 
 public interface IUserRepository : IGenericRepository<User>
 {
+    Task<IReadOnlyList<User>> GetByLevel(AccessLevel level);
+    Task<IReadOnlyList<User>> GetByGroup(int gid);
+    Task<IReadOnlyList<User>> GetByGroupAndLevel(int gid, AccessLevel level);
 }
