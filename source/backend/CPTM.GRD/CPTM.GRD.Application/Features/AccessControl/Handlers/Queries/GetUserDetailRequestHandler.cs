afc22bd38b08bdb35141244062d98108d263f6e3
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-14 13:13:36 -0300

    GetUserDetailRequest

---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/AccessControl/Handlers/Queries/GetUserDetailRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CPTM.GRD.Application.Contracts.Persistence.AccessControl;
+using CPTM.GRD.Application.DTOs.AccessControl;
+using CPTM.GRD.Application.Features.AccessControl.Requests.Queries;
+using MediatR;
+
+namespace CPTM.GRD.Application.Features.AccessControl.Handlers.Queries;
+
+public class GetUserDetailRequestHandler : IRequestHandler<GetUserDetailRequest, UserDto>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public GetUserDetailRequestHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<UserDto> Handle(GetUserDetailRequest request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.Get(request.Uid);
+        return _mapper.Map<UserDto>(user);
+    }
+}
---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Features/AccessControl/Requests/Queries/GetUserDetailRequest.cs
@@ -0,0 +1,9 @@
+﻿using CPTM.GRD.Application.DTOs.AccessControl;
+using MediatR;
+
+namespace CPTM.GRD.Application.Features.AccessControl.Requests.Queries;
+
+public class GetUserDetailRequest : IRequest<UserDto>
+{
+    public int Uid { get; set; }
+}
