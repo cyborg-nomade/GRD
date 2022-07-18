e10a902c4fe73a96c8938679630ae9523cc406cc
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-18 19:03:39 -0300

    created custom exceptions

---
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,8 @@
+﻿namespace CPTM.GRD.Application.Exceptions;
+
+public class BadRequestException : ApplicationException
+{
+    public BadRequestException(string message) : base(message)
+    {
+    }
+}
