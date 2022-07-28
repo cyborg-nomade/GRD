c25d57565f9d3e6fd2eb02e1bc42d44b8fd34430
Uriel Fiori <uriel.fiori@gmail.com>
2022-07-28 10:05:52 -0300

    fixed usuario ad init

--- source/backend/CPTM.GRD/CPTM.GRD.Application/Models/UsuarioAD.cs
+++ source/backend/CPTM.GRD/CPTM.GRD.Application/Models/UsuarioAD.cs
@@ -2,24 +2,17 @@
 
 public class UsuarioAD
 {
-    public UsuarioAD()
-    {
-        this.MembroDe = new List<string>();
-        this.ListasDeContato = new List<string>();
-        this.Subordinados = new List<string>();
-    }
-
     public string ObjectGUID { get; set; } = string.Empty;
 
-    public string Login { get; set; }
+    public string Login { get; set; } = string.Empty;
 
-    public string NomeLogon { get; set; }
+    public string NomeLogon { get; set; } = string.Empty;
 
-    public string OpcoesConta { get; set; }
+    public string OpcoesConta { get; set; } = string.Empty;
 
-    public string NomeContainer { get; set; }
+    public string NomeContainer { get; set; } = string.Empty;
 
-    public string NomeDistinto { get; set; }
+    public string NomeDistinto { get; set; } = string.Empty;
 
     public DateTime DataExpiracaoConta { get; set; }
 
@@ -29,7 +22,7 @@ public class UsuarioAD
 
     public DateTime DataUltimaAlteracaoSenha { get; set; }
 
-    public List<string> MembroDe { get; set; }
+    public List<string> MembroDe { get; set; } = new List<string>();
 
     public bool ContaBloqueada { get; set; }
 
@@ -41,83 +34,83 @@ public class UsuarioAD
 
     public bool SenhaNuncaExpira { get; set; }
 
-    public string ScriptLogon { get; set; }
+    public string ScriptLogon { get; set; } = string.Empty;
 
-    public string DiretorioBase { get; set; }
+    public string DiretorioBase { get; set; } = string.Empty;
 
-    public string UnidadeBase { get; set; }
+    public string UnidadeBase { get; set; } = string.Empty;
 
-    public string NomeExibicao { get; set; }
+    public string NomeExibicao { get; set; } = string.Empty;
 
-    public string Nome { get; set; }
+    public string Nome { get; set; } = string.Empty;
 
-    public string NomePrimeiro { get; set; }
+    public string NomePrimeiro { get; set; } = string.Empty;
 
-    public string NomeAbreviatura { get; set; }
+    public string NomeAbreviatura { get; set; } = string.Empty;
 
-    public string NomeUltimo { get; set; }
+    public string NomeUltimo { get; set; } = string.Empty;
 
-    public string Descricao { get; set; }
+    public string Descricao { get; set; } = string.Empty;
 
-    public string Anotacoes { get; set; }
+    public string Anotacoes { get; set; } = string.Empty;
 
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     public bool OcultarEmailListaContatosExchange { get; set; }
 
-    public List<string> ListasDeContato { get; set; }
+    public List<string> ListasDeContato { get; set; } = new List<string>();
 
-    public byte[] Foto { get; set; }
+    public byte[]? Foto { get; set; }
 
     public DateTime DataUltimoLogon { get; set; }
 
     public DateTime DataUltimoLogoff { get; set; }
 
-    public string PaisSigla { get; set; }
+    public string PaisSigla { get; set; } = string.Empty;
 
-    public string PaisNome { get; set; }
+    public string PaisNome { get; set; } = string.Empty;
 
-    public string PaisCodigo { get; set; }
+    public string PaisCodigo { get; set; } = string.Empty;
 
-    public string Cidade { get; set; }
+    public string Cidade { get; set; } = string.Empty;
 
-    public string Estado { get; set; }
+    public string Estado { get; set; } = string.Empty;
 
-    public string CodigoPostal { get; set; }
+    public string CodigoPostal { get; set; } = string.Empty;
 
-    public string Endereco { get; set; }
+    public string Endereco { get; set; } = string.Empty;
 
-    public string CaixaPostal { get; set; }
+    public string CaixaPostal { get; set; } = string.Empty;
 
-    public string Titulo { get; set; }
+    public string Titulo { get; set; } = string.Empty;
 
-    public string Escritorio { get; set; }
+    public string Escritorio { get; set; } = string.Empty;
 
-    public string Departamento { get; set; }
+    public string Departamento { get; set; } = string.Empty;
 
-    public string Empresa { get; set; }
+    public string Empresa { get; set; } = string.Empty;
 
-    public string GestorLogin { get; set; }
+    public string GestorLogin { get; set; } = string.Empty;
 
-    public string GestorNomeDistinto { get; set; }
+    public string GestorNomeDistinto { get; set; } = string.Empty;
 
-    public List<string> Subordinados { get; set; }
+    public List<string> Subordinados { get; set; } = new List<string>();
 
-    public string ClasseObjeto { get; set; }
+    public string ClasseObjeto { get; set; } = string.Empty;
 
-    public string TelefoneComercial { get; set; }
+    public string TelefoneComercial { get; set; } = string.Empty;
 
-    public string TelefoneResidencial { get; set; }
+    public string TelefoneResidencial { get; set; } = string.Empty;
 
-    public string TelefoneCelular { get; set; }
+    public string TelefoneCelular { get; set; } = string.Empty;
 
-    public string Fax { get; set; }
+    public string Fax { get; set; } = string.Empty;
 
     public string TelefoneIP { get; set; } = string.Empty;
 
-    public string Pager { get; set; }
+    public string Pager { get; set; } = string.Empty;
 
-    public string TelefoneResidencialOutro { get; set; }
+    public string TelefoneResidencialOutro { get; set; } = string.Empty;
 
     public class Filtro
     {
