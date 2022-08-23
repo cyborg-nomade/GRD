e38ca07e6904d4edd9e8d83b17406f7984b4091c
Uriel Fiori <uriel.fiori@gmail.com>
2022-08-23 09:09:00 -0300

    added route guards

--- source/frontend/cptm.grd.ui/pages/_app.tsx
+++ source/frontend/cptm.grd.ui/pages/_app.tsx
@@ -2,11 +2,13 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { wrapper } from "../services/redux/store";
 import { useAuth } from "../services/local-storage/auth-hook";
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import CssBaseline from "@mui/material/CssBaseline";
 import NavbarView from "components/nav/NavbarView";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { useAppSelector } from "services/redux/hooks";
+import { useRouter } from "next/router";
 
 const theme = createTheme({
     palette: {
@@ -25,6 +27,14 @@ const theme = createTheme({
 
 function MyApp({ Component, pageProps }: AppProps) {
     useAuth();
+    const authState = useAppSelector((state) => state.auth);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (authState.currentUser.id === 0) {
+            router.push("/login");
+        }
+    }, [authState.currentUser.id, router]);
 
     return (
         <React.Fragment>
