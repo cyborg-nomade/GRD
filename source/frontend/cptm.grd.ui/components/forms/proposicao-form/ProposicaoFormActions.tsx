939b00396dc2368f8d04e2ab03c92e75d39973d8
Uriel Fiori <uriel.fiori@gmail.com>
2022-08-30 15:25:50 -0300

    created handler funcs in proposicao form actions comp

--- source/frontend/cptm.grd.ui/components/forms/proposicao-form/ProposicaoFormActions.tsx
+++ source/frontend/cptm.grd.ui/components/forms/proposicao-form/ProposicaoFormActions.tsx
@@ -4,75 +4,126 @@ import FormActionSection from "../FormActionSection";
 import Grid from "@mui/system/Unstable_Grid";
 import { CreateProposicaoDto } from "models/proposicoes/proposicao.model";
 import { UseFormReturn } from "react-hook-form";
+import { useAppSelector } from "services/redux/hooks";
+import { useRouter } from "next/router";
+import { getFirstError } from "services/util/getFirstError";
 
 const ProposicaoFormActions = (props: {
     methods: UseFormReturn<CreateProposicaoDto>;
 }) => {
+    const authState = useAppSelector((state) => state.auth);
+    const router = useRouter();
+
+    const cancelarHandler = () => {
+        router.back();
+    };
+
     const cancelar = (
-        <Button variant="outlined" color="error">
+        <Button variant="outlined" color="error" onClick={cancelarHandler}>
             Cancelar
         </Button>
     );
 
-    const saveProposicaoHandler = (proposicao: CreateProposicaoDto) => {
+    const salvarHandler = (proposicao: CreateProposicaoDto) => {
         console.log(proposicao);
-        return {};
+
+        console.log(props.methods.formState.errors);
+        props.methods.trigger();
+
+        console.log(getFirstError(props.methods.formState.errors));
+        props.methods.setFocus(getFirstError(props.methods.formState.errors));
     };
 
     const salvar = (
         <Button
             variant="contained"
             color="success"
-            onClick={() => saveProposicaoHandler(props.methods.getValues())}
+            onClick={() => salvarHandler(props.methods.getValues())}
         >
             Salvar
         </Button>
     );
 
+    const removerHandler = () => {};
+
     const remover = (
-        <Button variant="contained" color="error">
+        <Button variant="contained" color="error" onClick={removerHandler}>
             Remover
         </Button>
     );
 
+    const enviarAprovacaoHandler = () => {};
+
     const enviarAprovacao = (
-        <Button variant="contained" color="warning">
+        <Button
+            variant="contained"
+            color="warning"
+            onClick={enviarAprovacaoHandler}
+        >
             Enviar Para Aprovação
         </Button>
     );
 
+    const retornarGrgHandler = () => {};
+
     const retornarGrg = (
-        <Button variant="contained" color="warning">
+        <Button
+            variant="contained"
+            color="warning"
+            onClick={retornarGrgHandler}
+        >
             Retornar à GRG
         </Button>
     );
 
+    const aprovarHandler = () => {};
+
     const aprovar = (
-        <Button variant="contained" color="success">
+        <Button variant="contained" color="success" onClick={aprovarHandler}>
             Aprovar
         </Button>
     );
 
+    const reprovarHandler = () => {};
+
     const reprovar = (
-        <Button variant="contained" color="error">
+        <Button variant="contained" color="error" onClick={reprovarHandler}>
             Reprovar
         </Button>
     );
 
+    const incluirEmPautaHandler = () => {};
+
     const incluirEmPauta = (
-        <Button variant="contained" color="success">
+        <Button
+            variant="contained"
+            color="success"
+            onClick={incluirEmPautaHandler}
+        >
             Incluir em Pauta
         </Button>
     );
 
+    const retornarADiretoriaHandler = () => {};
+
     const retornarADiretoria = (
-        <Button variant="contained" color="error">
+        <Button
+            variant="contained"
+            color="error"
+            onClick={retornarADiretoriaHandler}
+        >
             Retornar à Diretoria
         </Button>
     );
 
+    const ajustesRealizadosHandler = () => {};
+
     const ajustesRealizados = (
-        <Button variant="contained" color="success">
+        <Button
+            variant="contained"
+            color="success"
+            onClick={ajustesRealizadosHandler}
+        >
             Ajustes Realizados
         </Button>
     );
